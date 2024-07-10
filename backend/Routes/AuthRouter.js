d3807569835516ae9bd410c9b426aadb75706995
Priyom1 <soikatmirror23@gmail.com>
2024-07-10 14:50:31 +0600

    Jwt Token

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,15 +1,10 @@
 const { signup } = require('../Controllers/AuthController');
-const { signupValidation } = require('../Middlewares/AuthValidation');
+const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 
 const router=require('express').Router();
 
-router.post('/login',(req,res)=>{
-    res.send('Login Successful');
-});
 
-router.post('/signup',(req,res)=>{
-    res.send('Signup Successful');
-});
+router.post('/signup',loginValidation,signup)
 router.post('/signup',signupValidation,signup);
 
 module.exports=router;
